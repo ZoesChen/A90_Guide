baa7b917ce9fdecf3b71eee06fe854c389486a0b
Zoe Chen <zoe.chen@morninghan.com>
2017-03-13 13:14:06 +0800

    Add queue ande playThread to handle playCmd

--- packages/example/App/Guide/guide.c
+++ packages/example/App/Guide/guide.c
@@ -5,58 +5,129 @@
 #include <fcntl.h>
 #include <pthread.h>
 #include "key.h"
-
+#include "queue.h"
+/*
+ * Define
+ * */
 #define MAX_MUSIC_INDEX 4 // 4bit num -> 1 music num
+#define PLAYCMD_INIT_STAT -1
 
+/*
+ * Parameter
+ * */
 static pthread_t keyThread;
+static pthread_t playThread;
 static int readKeyFlag = 0;
-static int readCount = 100;
 
 #define DEBUG_X86
 //X86_ESC: stop read
 //X86_ZERO: put music num into queue
 
-int music_num[4] = {0};
+static int music_num[4] = {0};
+static int musicNum = -1;
+queue keyQueue;
+queue locationQueue;
+pthread_cond_t playThreadCond;
+pthread_mutex_t playThreadLock;
 
 #ifdef DEBUG_X86
-#define X86_ESC   1
-#define X86_KEY1 2
-#define X86_KEY2 3
-#define X86_KEY3 4
-#define X86_KEY4 5
-#define X86_KEY5 6
-#define X86_KEY6 7
-#define X86_KEY7 8
-#define X86_KEY8 9
-#define X86_KEY9 10
-#define X86_ZERO 11
-#define X86_KEY_A 30
-
 static int oldKey = -1;
 static int newKey = -1;
 #endif
 
+/*
+ * Struct & Enum
+ * */
+enum QueueCondition
+{
+	KEYQUEUE_FREE,
+	LOCATIONQUEUE_FREE,
+	BOTH_FREE,
+	BOTH_BUSY,
+	INVALID
+};
 
-int Init()
+static enum QueueCondition whichQueueFree()
+{
+	enum QueueCondition qc = INVALID;
+	if (IsQueueFree(&keyQueue) && IsQueueFree(&locationQueue)) {
+		qc = BOTH_FREE;
+	} else if (IsQueueFree(&keyQueue)) {
+		qc = KEYQUEUE_FREE;
+	} else if (IsQueueFree(&locationQueue)) {
+		qc = LOCATIONQUEUE_FREE;
+	} else {
+		qc = BOTH_BUSY;
+	}
+	return qc;
+}
+
+static void InitQueue()
+{
+	keyQueue.front = NULL;
+	keyQueue.end = NULL;
+	locationQueue.front = NULL;
+	locationQueue.end = NULL;
+}
+
+static int Init()
 {
 	int res;
 	res = OpenKeyDev();
 	if(res) {
 		return -1;
 	}
+	
+	InitQueue();
+	
 	readKeyFlag = 1;
 	return 0;
 }
 
+void *PlayThreadHandle(void *arg)
+{
+	int playCmd = PLAYCMD_INIT_STAT;
+	while(readKeyFlag) {
+		pthread_mutex_lock(&playThreadLock);
+		switch(whichQueueFree()){
+			case BOTH_FREE:
+				printf("BOTH_FREE\n");
+				pthread_cond_wait(&playThreadCond, &playThreadLock);
+				printf("Wake up by cond\n");
+			break;
+			case BOTH_BUSY:
+				printf("BOTH_BUSY\n");
+				playCmd = Pop(&keyQueue);
+			break;
+			case KEYQUEUE_FREE:
+			//keyqueue NULL, locationQueue not NULl
+				printf("KEYQUEUE_FREE\n");
+				playCmd = Pop(&locationQueue);
+			break;
+			case LOCATIONQUEUE_FREE:
+				printf("LOCATIONQUEUE_FREE\n");
+				playCmd = Pop(&keyQueue);
+			break;
+			default:
+			break;
+		}
+		pthread_mutex_unlock(&playThreadLock);
+		if (playCmd != PLAYCMD_INIT_STAT) {
+			//Handle playCmd && reset playCmd
+			printf("PlayThreadHandle: playCmd = %d\n", playCmd);
+			playCmd = PLAYCMD_INIT_STAT;
+		}
+	}
+	return NULL;
+}
+
 void *KeyThreadHandle(void *arg)
 {
-	printf("Enter into KeyThreadHandle\n");
 	int keyCode;
 	int musicNumIndex = 0;
 #ifdef DEBUG_X86
 	int x86StartFlag = 0;
 #endif
-	printf("readKeyFlag = %d, readCount = %d\n", readKeyFlag, readCount);
 	while(readKeyFlag) {
 		ReadKey(&keyCode);
 #ifdef DEBUG_X86
@@ -67,39 +138,42 @@ void *KeyThreadHandle(void *arg)
 		if (x86StartFlag != 1) {
 			continue;
 		}
-			oldKey = newKey;
-			newKey = keyCode;
-			if (oldKey == newKey)
-				continue;
-#endif	
+
+		oldKey = newKey;
+		newKey = keyCode;
+		if (oldKey == newKey)
+			continue;
 
 		if (keyCode == X86_ESC) {
 			printf("Press esc\n");
 			readKeyFlag = 0;
 			continue;
 		}
-		
+#endif
 		if (keyCode >= X86_KEY1 && keyCode <= X86_ZERO) {
-			//printf("musicNumIndex = %d\n",  musicNumIndex);
-			
 			if (musicNumIndex < MAX_MUSIC_INDEX) {
 				if (keyCode == X86_ZERO)
 					music_num[musicNumIndex] = 0;
 				else
 					music_num[musicNumIndex] =keyCode -1;
-				//printf("music_num[%d] = %d", musicNumIndex, music_num[musicNumIndex]);
+
 				musicNumIndex++;
 			}
 
 			if (musicNumIndex == MAX_MUSIC_INDEX) {
-				musicNumIndex = 0;
 				int i = 0;
-				printf("MusicNum = %d\n", music_num[0]*1000 + music_num[1]*100 + music_num[2]*10 + music_num[3]);
+				
+				musicNum = music_num[0]*1000 + music_num[1]*100 + music_num[2]*10 + music_num[3];
+				musicNumIndex = 0;
+				printf("MusicNum = %d\n", musicNum);
 				for(i = 0; i < MAX_MUSIC_INDEX; i++) {
 					music_num[i] = 0;
 				}
 				//Enqueue music num into keyQueue
-				//ToDo
+				pthread_mutex_lock(&playThreadLock);
+				Push(musicNum, &keyQueue);
+				pthread_cond_signal(&playThreadCond);
+				pthread_mutex_unlock(&playThreadLock);
 			}
 		}
 	}
@@ -112,13 +186,13 @@ void *KeyThreadHandle(void *arg)
 
 int main(int argv, char **argc)
 {
-	int res;
 	printf("Begin......\n");
 	if (Init() < 0) {
 		return -1;
 	}
 
-	res = pthread_create(&keyThread, NULL, KeyThreadHandle, NULL);
+	pthread_create(&keyThread, NULL, KeyThreadHandle, NULL);
+	pthread_create(&playThread, NULL, PlayThreadHandle, NULL);
 	pthread_join(keyThread, NULL);
 	return 0;
 }
